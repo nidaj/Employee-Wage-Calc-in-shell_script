951b3b2c2575750d7e084603652397fdba37248f
nida.j <nida.jawre@gmail.com>
2020-10-31 20:32:24 +0530

    Employee present absent calculated

--- q1.sh
+++ q1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
+attendance=$((RANDOM%2))
+if (( $attendance == 1 ))
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
+
 
